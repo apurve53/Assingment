e51b5ec4814141feccd6cf4a1a94f55e0c1a2eae
apurve53 <147850445+apurve53@users.noreply.github.com>
2024-06-05 07:30:23 +0530

    Test for CICD
    I have just add a file to see if cloning process on ec2 machine is working or not I will check the Action Logs after pushing this to remote

--- server.js
+++ server.js
@@ -13,49 +13,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
-// app.post('/order', async (req, res) => {
-//   const { buyer, qty, price } = req.body;
-//   const transaction = await sequelize.transaction();
-//   try {
-//     const pendingOrders = await PendingOrder.findAll({ lock: true, transaction });
-//     let remainingQty = qty;
-//     for (let order of pendingOrders) {
-//       //Move matched order to completed order
-//       if (buyer && price >= order.sellerPrice) {
-//         const matchedQty = Math.min(remainingQty, order.sellerQty);
-//         await CompletedOrder.create({
-//           price: order.sellerPrice,
-//           quantity: matchedQty
-//         }, { transaction });
-
-//         if (order.sellerQty > remainingQty) {
-//           await order.update({ sellerQty: order.sellerQty - remainingQty }, { transaction });
-//           remainingQty = 0;
-//           break; // All buyer quantity is matched
-//         } else {
-//           remainingQty -= order.sellerQty;
-//           await order.destroy({ transaction });
-//         }
-//       }
-//     }
-
-//     //if there is remaining quantity add it to the Panding order table.
-//     if (remainingQty > 0) {
-//       await PendingOrder.create({
-//         buyerQty: remainingQty,
-//         buyerPrice: price,
-//         sellerPrice: price + 1, // Placeholder price, adjust as necessary
-//         sellerQty: 0
-//       }, { transaction });
-//     }
-//     await transaction.commit();
-//     res.status(200).send('Order processed successfully');
-
-//   } catch (e) {
-//     await transaction.rollback();
-//     res.status(500).send('Error processing order: ' + error.message);
-//   }
-// })
 app.post('/upload', upload.single('file'), async (req, res) => {
   const file = req.file;
   const workbook = XLSX.readFile(file.path);
