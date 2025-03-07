1b09a06a4412955406dbadb857cdfb3854ebde80
Apurve Srivastava <apurve2014@gmail.com>
2025-03-07 05:52:31 +0530

    Change Os Module Now it will work on any Opreting System testing not done yet

--- osModule.js
+++ osModule.js
@@ -3,12 +3,29 @@ const localAddress = {
     publicIp: "",
     runningIp: "",
     setAddress: function () {
+        console.log("os.networkInterfaces() : ", os.networkInterfaces())
+        // if (this.runningIp == "") {
+        //     let netList = os.networkInterfaces().Ethernet;
+        //     for (let i = 0; i < netList?.length; i++) {
+        //         if (netList[i].family == 'IPv4') {
+        //             this.runningIp = netList[i].address;
+        //             break;
+        //         }
+        //     }
+        //     return this.runningIp;
+        // } else {
+        //     return this.runningIp;
+        // }
         if (this.runningIp == "") {
-            let netList = os.networkInterfaces().Ethernet;
-            for (let i = 0; i < netList?.length; i++) {
-                if (netList[i].family == 'IPv4') {
-                    this.runningIp = netList[i].address;
-                    break;
+            const data = os.networkInterfaces();
+            for (const interfaceKey in data) {
+                if (data.hasOwnProperty(interfaceKey)) {
+                    const interfaceData = data[interfaceKey];
+                    interfaceData.forEach(entry => {
+                        if (entry.family === 'IPv4' && entry.mac !== '00:00:00:00:00:00') {
+                            this.runningIp = entry.address;
+                        }
+                    });
                 }
             }
             return this.runningIp;
